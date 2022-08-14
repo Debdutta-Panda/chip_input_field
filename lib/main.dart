ca75e41fd5d5e8610de0a12ee8616f066e106bb9
Debdutta Panda <me@debduttapanda.com>
2022-08-14 21:19:19 +0530

    mac 14 aug 2022 2119

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+
+import 'chip_input_field.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,516 +44,84 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.maxFinite,
-        child: Padding(
-          padding: const EdgeInsets.only(right: 150),
-          child: Column(
-            children: [
-              Container(
-                width: double.maxFinite,
-                child: ChipInputField(
-                  controller: controller,
-                  autocomplete: true,
-                  fieldMinHeight: 60,
-                  fieldPadding: EdgeInsets.all(8),
-                  tagBuilder: (data,fnDelete)=>Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(data),
-                      IconButton(
-                          onPressed: (){
-                            fnDelete(data);
-                          },
-                          icon: Icon(Icons.close
-                          ))
-                    ],
-                  ),
-                  optionViewBuilder: (highlighted,index)=>Text(
-                      index.toString(),
-                    style: TextStyle(
-                      color: highlighted ? Colors.red : Colors.black
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.maxFinite,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 150),
+            child: Column(
+              children: [
+                Container(
+                  width: double.maxFinite,
+                  child: ChipInputField(
+                    controller: controller,
+                    autocomplete: true,
+                    optionBuilder: (query){
+                      if(query.text.isEmpty){
+                        return [];
+                      }
+                      print("1111111");
+                      print(query.text);
+                      return [
+                        query.text+"1",
+                        query.text+"2",
+                        query.text+"3",
+                        query.text+"4",
+                      ];
+                    },
+                    fieldMinHeight: 60,
+                    fieldPadding: EdgeInsets.all(8),
+                    tagBuilder: (data,fnDelete)=>Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(data),
+                        IconButton(
+                            onPressed: (){
+                              fnDelete(data);
+                            },
+                            icon: Icon(Icons.close
+                            ))
+                      ],
                     ),
-                  ),
-                  optionListMaterialBuilder: (child,elevation)=>Material(
-                    color: Colors.green,
-                    elevation: elevation,
-                    child: child,
-                  ),
-                  onItemWillBeDeleted: (item)=>true,
-                  onTagAdded: (item){
-
-                  },onTagDeleted: (item){
-
-                  },
-              ),
-              ),
-              ElevatedButton(
-                  onPressed: (){
-                    controller.addItem("123");
-                  },
-                  child: Text("Focus")
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-////////////////////////////////////////////
-class ChipInputController{
-  Function()? onRequestFocus;
-  Function()? onRemoveFocus;
-  Function(String)? onSetText;
-  String Function()? onGetText;
-  List<dynamic> Function()? onGetItems;
-  Function(List<dynamic>)? onSetItems;
-
-  Function(dynamic)? onAddItem;
-  Function(dynamic)? onDeleteItem;
-  Function(dynamic,int)? onInsertItem;
-  Function(dynamic,dynamic)? onInsertAfter;
-  Function(int)? onRemoveAt;
-
-  void removeAt(int index){
-    onRemoveAt?.call(index);
-  }
-
-  void addItem(dynamic item){
-    onAddItem?.call(item);
-  }
-
-  void deleteItem(dynamic item){
-    onDeleteItem?.call(item);
-  }
-  void insertItem(dynamic item, int index){
-    onInsertItem?.call(item,index);
-  }
-  void insertAfter(dynamic item, dynamic after){
-    onInsertAfter?.call(item,after);
-  }
-
-  void requestFocus(){
-    onRequestFocus?.call();
-  }
-  void removeFocus(){
-    onRemoveFocus?.call();
-  }
-  set text(String value){
-    onSetText?.call(value);
-  }
-  String get text{
-    return onGetText?.call()??"";
-  }
-  List<dynamic> get items{
-    return onGetItems?.call()??[];
-  }
-  set items(List<dynamic> list){
-    onSetItems?.call(list);
-  }
-}
-class ChipInputField extends StatefulWidget {
-  const ChipInputField(
-      {
-        this.autocomplete = false,
-        this.fieldMinHeight = 60,
-        this.fieldDecoration,
-        this.fieldPadding = const EdgeInsets.all(8.0),
-        this.spacing = 10,
-        this.runSpacing = 10,
-        this.tagBuilder,
-        this.coreFieldMinWidth = 100,
-        this.optionViewBuilder,
-        this.optionListTopPadding = 16,
-        this.optionListElevation = 4,
-        this.optionListConstraints,
-        this.optionListPadding = EdgeInsets.zero,
-        this.optionListMaterialBuilder,
-        this.optionBuilder = _defaultOptionBuilder,
-        this.coreFieldHeight = 32,
-        this.hint = "",
-        this.textFieldWrapperBuilder,
-        this.inputFormatters,
-        required this.onTagAdded,
-        required this.onTagDeleted,
-        required this.onItemWillBeDeleted,
-        this.onChange,
-        this.controller,
-        Key? key
-      }
-  ) : super(key: key);
-  final bool autocomplete;
-  final double fieldMinHeight;
-  final BoxDecoration? fieldDecoration;
-  final EdgeInsets fieldPadding;
-  final double spacing;
-  final double runSpacing;
-  final Widget Function(dynamic,Function(dynamic))? tagBuilder;
-  final double coreFieldMinWidth;
-  final Widget Function(bool,int)? optionViewBuilder;
-  final double optionListTopPadding;
-  final double optionListElevation;
-  final BoxConstraints? optionListConstraints;
-  final EdgeInsets optionListPadding;
-  final Material Function(Widget,double)? optionListMaterialBuilder;
-  final Iterable<Object> Function(TextEditingValue) optionBuilder;
-  final double coreFieldHeight;
-  final String hint;
-  final Widget Function(Widget)? textFieldWrapperBuilder;
-  final List<TextInputFormatter>? inputFormatters;
-  final Function(dynamic) onTagAdded;
-  final Function(dynamic) onTagDeleted;
-  final bool Function(dynamic) onItemWillBeDeleted;
-  final Function(String)? onChange;
-  final ChipInputController? controller;
-  @override
-  State<ChipInputField> createState() => _ChipInputFieldState();
-
-  static Iterable<Object> _defaultOptionBuilder(TextEditingValue value){
-    return Iterable<Object>.empty();
-  }
-}
-
-class _ChipInputFieldState extends State<ChipInputField> {
-  var focusNode = FocusNode();
-  var controller = TextEditingController();
-  List<dynamic> items = [];
-  @override
-  Widget build(BuildContext context) {
-    return
-    LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints)=>Container(
-          child: GestureDetector(
-            onTap: onTap,
-            child: MouseRegion(
-              cursor: SystemMouseCursors.text,
-              child: Container(
-                constraints: BoxConstraints(
-                    minHeight: widget.fieldMinHeight
+                    optionViewBuilder: (highlighted,index,option)=>Text(
+                        option.toString(),
+                      style: TextStyle(
+                        color: highlighted ? Colors.red : Colors.black
+                      ),
+                    ),
+                    optionListMaterialBuilder: (child,elevation)=>Material(
+                      color: Colors.green,
+                      elevation: elevation,
+                      child: child,
+                    ),
+                    onItemWillBeDeleted: (item)=>true,
+                    onTagAdded: (item){
+                      print("added=$item");
+                    },onTagDeleted: (item){
+                      print("deleted=$item");
+                    },
+                    onTagsAdded: (list){
+                      print("addeds=$list");
+                    },
+                    onChange: (value){
+                      //print(value);
+                    },
                 ),
-                decoration: widget.fieldDecoration ?? _defaultFieldDecoration(),
-                child: Padding(
-                  padding: widget.fieldPadding,
-                  child: Wrap(
-                    runSpacing: widget.runSpacing,
-                    spacing: widget.spacing,
-                    alignment: WrapAlignment.start,
-                    runAlignment: WrapAlignment.center,
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      ...items.map(
-                              (e) => widget.tagBuilder?.call(e,onDelete)?? _defaultChipWidget(e)
-                      ).toList(),
-                      IntrinsicWidth(
-                        child: Container(
-                          constraints: BoxConstraints(
-                              minWidth: widget.coreFieldMinWidth
-                          ),
-                          child: RawKeyboardListener(
-                            focusNode: FocusNode(),
-                            onKey: onKey,
-                            child:
-                            widget.autocomplete ?
-                            AutocompleteWidget()
-                            :(widget.textFieldWrapperBuilder?.call(TextFieldWidget()) ?? TextFieldWidget()),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
                 ),
-              ),
+                ElevatedButton(
+                    onPressed: (){
+                      controller.addItem("123");
+                    },
+                    child: Text("Focus")
+                )
+              ],
             ),
           ),
-        )
-    );
-
-  }
-
-  Widget _defaultChipWidget(dynamic e){
-    return Container(
-      height: 32,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: Theme.of(context).colorScheme.secondary
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: IntrinsicWidth(
-          child: Row(
-            children: [
-              Expanded(
-                child: Text(
-                  e.toString(),
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.onPrimary
-                  ),
-                ),
-              ),
-              SizedBox(width: 8,),
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(100),
-                  onTap: ()=>onDelete(e),
-                  child: Icon(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    Icons.close,
-                    size: 16,
-                  ),
-                ),
-              )
-            ],
-          ),
         ),
       ),
     );
-  }
-
-  Widget AutocompleteWidget(){
-    return Autocomplete<Object>(
-      optionsViewBuilder: (
-          BuildContext context,
-          AutocompleteOnSelected<Object> onSelected,
-          Iterable<Object> options
-          ) {
-        return
-          Padding(
-            padding: EdgeInsets.only(top: widget.optionListTopPadding),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: widget.optionListMaterialBuilder?.call(optionListWidget(options,onSelected),widget.optionListElevation) ?? Material(
-                elevation: widget.optionListElevation,
-                child: optionListWidget(options,onSelected),
-              ),
-            ),
-          );
-      },
-      optionsBuilder: widget.optionBuilder,
-      fieldViewBuilder: (BuildContext context,
-          TextEditingController textEditingController,
-          FocusNode fn, VoidCallback onFieldSubmitted) {
-        controller = textEditingController;
-        focusNode = fn;
-        return SizedBox(
-          height: widget.coreFieldHeight,
-          child: Center(
-            child: widget.textFieldWrapperBuilder?.call(_defaultTextFormField(textEditingController,onFieldSubmitted,fn)) ??
-                _defaultTextFormField(textEditingController,onFieldSubmitted,fn),
-          ),
-        );
-      },
-      onSelected: (dynamic selection) {
-        onComma(selection);
-      },
-    );
-  }
-
-  Widget _defaultTextFormField(
-      TextEditingController textEditingController,
-      VoidCallback onFieldSubmitted,
-      FocusNode fn
-  ){
-    return TextFormField(
-        controller: textEditingController,
-        decoration: InputDecoration.collapsed(
-            hintText: widget.hint
-        ),
-        focusNode: fn,
-        onFieldSubmitted: (String value) {
-          onFieldSubmitted();
-        },
-        inputFormatters: widget.inputFormatters ?? [
-          FilteringTextInputFormatter.allow(RegExp(r"[^,^\s]+[\s]?")),
-        ],
-      onChanged: (value)=>widget.onChange?.call(value),
-    );
-  }
-
-  Widget optionListWidget(Iterable<Object> options, AutocompleteOnSelected<Object> onSelected){
-    return ConstrainedBox(
-      constraints: widget.optionListConstraints ?? _defaultOptionListConstraints(),
-      child: ListView.builder(
-        padding: widget.optionListPadding,
-        shrinkWrap: true,
-        itemCount: options.length,
-        itemBuilder: (BuildContext context, int index) {
-          final Object option = options.elementAt(index);
-          return InkWell(
-            onTap: () {
-              onSelected(option);
-            },
-            child: Builder(
-                builder: (BuildContext context) {
-                  final bool highlight = AutocompleteHighlightedOption.of(context) == index;
-                  if (highlight) {
-                    SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
-                      Scrollable.ensureVisible(context, alignment: 0.5);
-                    });
-                  }
-                  return widget.optionViewBuilder?.call(highlight,index) ?? _defaultOptionWidget(highlight,index);
-                }
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _defaultOptionWidget(bool highlight, dynamic option){
-    return Container(
-      color: highlight ? Theme.of(context).focusColor : null,
-      padding: const EdgeInsets.all(16.0),
-      child: Text(option.toString()),
-    );
-  }
-
-  void onTap(){
-    focusNode.requestFocus();
-    controller.selection = TextSelection.collapsed(offset: controller.text.length);
-  }
-
-  void onKey(RawKeyEvent event){
-    if(event.runtimeType == RawKeyUpEvent){
-      var key = event.logicalKey.keyId;
-      switch(key){
-        case 44: onComma(null);break;
-        default: print(key);
-      }
-    }
-    else if(event.runtimeType == RawKeyDownEvent){
-      var key = event.logicalKey.keyId;
-      switch(key){
-        case 4294967304: onBackSpace();break;
-        default: print(key);
-      }
-    }
-  }
-
-  void onComma(selection) {
-    var text = controller.text.trim();
-    if(text.isEmpty){
-      return;
-    }
-    controller.text = "";
-    setState(() {
-      items.add(text);
-      widget.onTagAdded(selection??text);
-    });
-    onTap();
-  }
-
-  void onBackSpace() {
-    var selection = controller.selection;
-    var start = selection.start;
-    var end = selection.end;
-    if(start<1 && end<1 && items.isNotEmpty){
-      var last = items.last;
-      if(!widget.onItemWillBeDeleted(last)){
-        return;
-      }
-      setState(() {
-        items.removeLast();
-        widget.onTagDeleted(last);
-      });
-    }
-  }
-
-  void onDelete(dynamic e) {
-    if(!widget.onItemWillBeDeleted(e)){
-      return;
-    }
-    setState(() {
-      items.remove(e);
-      widget.onTagDeleted(e);
-    });
-  }
-
-  TextFieldWidget() {
-    return TextField(
-        decoration: InputDecoration.collapsed(
-            hintText: widget.hint
-        ),
-        focusNode: focusNode,
-        controller: controller,
-        inputFormatters: widget.inputFormatters?? [
-          FilteringTextInputFormatter.allow(RegExp(r"[^,^\s]+[\s]?")),
-        ],
-      onChanged: (value)=>widget.onChange?.call(value),
-    );
-  }
-
-  BoxDecoration _defaultFieldDecoration() {
-    return BoxDecoration(
-        border: Border.all(color: Theme.of(context).colorScheme.primary)
-    );
-  }
-
-  BoxConstraints _defaultOptionListConstraints() {
-    return BoxConstraints(maxHeight: 200,maxWidth: 200);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    widget.controller?..onSetItems = (items){
-      setState(() {
-        this.items = items;
-      });
-    }
-    ..onGetItems = (){
-      return this.items;
-    }
-    ..onGetText = (){
-      return controller.text;
-    }
-    ..onSetText = (value){
-      controller.text = value;
-    }
-    ..onRemoveFocus = (){
-      focusNode.unfocus();
-    }
-    ..onRequestFocus = (){
-      focusNode.requestFocus();
-    }
-    ..onAddItem = (item){
-      setState(() {
-        items.add(item);
-      });
-    }
-    ..onDeleteItem = (item){
-      if(items.contains(item)){
-        setState(() {
-          items.remove(item);
-        });
-      }
-    }
-    ..onInsertItem = (item,index){
-      if(items.length>index-1){
-        setState(() {
-          items.insert(index, item);
-        });
-      }
-    }
-    ..onInsertAfter = (item,after){
-      var index = items.indexOf(item);
-      if(index > -1){
-        setState(() {
-          items.insert(index+1, item);
-        });
-      }
-    }
-    ..onRemoveAt = (index){
-      if(items.length>index&&index > -1){
-        setState(() {
-          items.removeAt(index);
-        });
-      }
-    }
-    ;
   }
 }
+
 
